c7cd8c1c3e4d06858f9c8c07fa08819300360ae4
LeeGunhyoung <yuby1003@naver.com>
2023-06-07 15:56:43 +0900

    app-8

--- app/src/main/java/bitmyapp/App.java
+++ app/src/main/java/bitmyapp/App.java
@@ -8,11 +8,25 @@ public class App {
 
 
   public static void main(String[] args) {
-    MemberHandler.inputMembers();
+    goMainMenu();
+    System.out.println("안녕하 가세요!");
 
-    System.out.println();
+    Prompt.close();
+  }
+  private static void goMainMenu() {
+    while(true) {      
+      System.out.println("1. 회원관리");
+      System.out.println("9. 종료");
+      int menuNo = Prompt.inputInt("메뉴> ");
 
-    MemberHandler.printMembers();
+      if (menuNo == 1) {
+        MemberHandler.service();
+      } else if(menuNo == 9){
+        break;
+      } else {
+        System.out.println("잘못된 메뉴 번호 입니다.");
+      }
+    }
   }
 
 
